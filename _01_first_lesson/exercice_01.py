e2f9fd6eacf76901fac1f9ca6aef7f0530b250e8
Akcel77 <Akeucel@gmail.com>
2022-10-10 15:10:20 +0200

    Python ex & soluce done 1 to 14

--- _01_first_lesson/exercice_01.py
+++ _01_first_lesson/exercice_01.py
@@ -23,5 +23,35 @@
 # si l’âge tapé est supérieur ou égale à 18 et le message « vous êtes mineur ! » si l’âge tapé est inférieur à 18
 
 
-# if elif else
+# if elif else / double condition
 # 7 - Ecrire un programme en Python qui demande à l'utilisateur de saisir 3 nombre x, y et z et de lui afficher leur maximum
+
+
+# for in range 
+# 8 - Ecrire un programme en Python qui demande à l'utilisateur de saisir un nombre entier n et de lui afficher la valeur de la somme 1 + 2 + … + n =
+
+
+# for in range
+# 9 - Ecrire un programme en Python qui demande à l'utilisateur de saisir un nombre entier n et de lui afficher n !
+
+
+# ** racine
+# 10 - Ecrire un programme en Python qui demande à l’utilisateur de saisir le rayon d'un cercle et de lui renvoyer la surface et le périmètre.
+
+
+# for in range / condition
+# 11 - Ecrire un programme en Python qui demande à l’utilisateur de saisir un nombre entier n et de lui afficher tous les diviseurs de ce nombre.
+
+
+# for in for (double boucle)
+# 12 -
+#  1) – Ecrire un programme en Python qui demande à l’utilisateur de saisir un nombre entier n et de lui afficher la table de multiplication de ce nombre.
+#  2) – Améliorez le programme afin qu’il affiche les tables de multiplications de tous les nombres compris entre 1 et 9
+
+
+# // et % 
+# 13 - Ecrire un programme en langage Python qui demande à l'utilisateur de saisir deux nombres entiers a et b et de lui afficher le quotient et le reste de la division euclidienne de a par b.
+
+
+# for et if else 
+# 14 - Ecrire un programme en langage Python qui demande à l’utilisateur de saisir un nombre entier n et de lui afficher si ce nombre est carré parfait ou non.
--- _01_first_lesson/solution_01.py
+++ _01_first_lesson/solution_01.py
@@ -1,49 +1,49 @@
-# # 1 - demander à l'utilisateur de saisir son nom
-# name = input("Saisir votre nom : ")
-# print("Bienvenue : " , name)
+# 1 - demander à l'utilisateur de saisir son nom
+name = input("Saisir votre nom : ")
+print("Bienvenue : " , name)
 
-# # 2 - demander à l'utilisateur de saisir les valeurs de a et de b
-# a = input("Tapez la valeur du nombre a : ")
-# b = input("Tapez la valeur du nombre b : ")
-# # Convertir les chaines de caractères en entier
-# a = int(a)
-# b = int(b)
-# s = a+b
-# print("La somme de a et de b est a + b = " , s)
+# 2 - demander à l'utilisateur de saisir les valeurs de a et de b
+a = input("Tapez la valeur du nombre a : ")
+b = input("Tapez la valeur du nombre b : ")
+# Convertir les chaines de caractères en entier
+a = int(a)
+b = int(b)
+s = a+b
+print("La somme de a et de b est a + b = " , s)
 
-# # 3 - lire les valeurs de a et b
-# a = int(input("Tapez la valeur du nombre a : "))
-# b = int(input("Tapez la valeur du nombre b : "))
+# 3 - lire les valeurs de a et b
+a = int(input("Tapez la valeur du nombre a : "))
+b = int(input("Tapez la valeur du nombre b : "))
 
-# # Faire un test de comparaison pour trouver le plus grand 
-# if (a > b):
-#     print("Le maximum  de a et de b est : a = ", a)
-# else:
-#     print("Le maximum  de a et de b est : b = ", b)
+# Faire un test de comparaison pour trouver le plus grand 
+if (a > b):
+    print("Le maximum  de a et de b est : a = ", a)
+else:
+    print("Le maximum  de a et de b est : b = ", b)
 
-# # 4 - # parcourir les 100 premiers nombres à l'aide de la boucle for
-# for i in range(0,101):
-#     print(i)
-
-
-# # 5 - # Lire la valeur de n
-# n = input("Type value of the integer n : ")
-
-# # Convertir n en entier
-# n = int(n)
-# # Tester si n est pair ou non
-# if(n%2 == 0):
-#     print("Le nombre '", n, "' tapé est pair ")
-# else:
-#     print("Le nombre '", n, "' tapé est impair ")
+# 4 - # parcourir les 100 premiers nombres à l'aide de la boucle for
+for i in range(0,101):
+    print(i)
 
 
-# # 6 - Demander à l'utilisateur de taper son âge 
-# age = int(input("Tapez votre age : "))
-# if(age > 18):
-#     print("Vous êtes majeur !")
-# else:
-#     print("Vous êtes mineur !")
+# 5 - # Lire la valeur de n
+n = input("Type value of the integer n : ")
+
+# Convertir n en entier
+n = int(n)
+# Tester si n est pair ou non
+if(n%2 == 0):
+    print("Le nombre '", n, "' tapé est pair ")
+else:
+    print("Le nombre '", n, "' tapé est impair ")
+
+
+# 6 - Demander à l'utilisateur de taper son âge 
+age = int(input("Tapez votre age : "))
+if(age > 18):
+    print("Vous êtes majeur !")
+else:
+    print("Vous êtes mineur !")
 
 
 # 7 - # Demander à l'utilisateur de taper 3 nombres  a, b, c
@@ -62,3 +62,79 @@ elif(c > a and c > b):
 else: 
     max = max
 print("Le maximum des trois nombre est : max(a,b,c) = ", max)
+
+# 8 - Demander à l'utilisateur de saisir la valeur de l'entier n
+n = int(input("Type a value of the integer n "))
+
+# define et initialiser une variable auxiliaire j
+j = 0
+for i in range(1,n+1):
+    j = j + i 
+print("La somme  1 + 2 + 3 + ...+ ",n," = : ", j)
+
+
+# - 9 Demander à l'utilisateur de saisir la valeur de l'entier n
+n = int(input("Type a value of the integer n "))
+
+# define et initialiser une variable auxiliaire j
+j = 1
+for i in range(1,n+1):
+    j = j*i 
+print("Factorielle de n est : ",n,"! = : ", j)
+
+# 10 - importer le nombre pi à partir de la bibliothèque math
+from math import pi
+# lire la valeur du rayon r
+r = int(input("Saisissez la valeur du rayon r: "))
+# calcul du périmètre du cercle
+P = 2*pi*r
+# calcul de la surface du cercle
+S = pi*(r**2)
+print("Le périmètre du cercle de rayons r =",r," est P = ", P)  
+print("La surface du cercle de rayons r =",r," est S = ", S)
+
+
+# 11 - Recuperer la valeur de l'entier
+n = int(input("Tapez la valeur de l'entier n "))
+# parcourir tous les entiers inféreir ou égale à n
+for i in range(1,n+1):
+    # tester si i est un diviseur de n
+    if(n%i==0):
+        print("Le nombre ",i," est un diviseur de  ",n)
+
+
+# 12 - Partie 1
+# Lire la valeur de l'entier n
+n = int(input("Tapez la valeur de n "))
+print("La table de multiplication de : ", n," est :")
+for i in range(1,10):
+    print(i , " x ", n, " = ",i*n)
+# 12 - Partie 2
+for n in range(1,10):
+    #insert separator
+    print("--------------------------------------")
+    print("la table de multiplication de  : ", n," est :")
+    for i in range(1,10):
+        print(i , " x ", n, " = ",i*n)
+
+
+# 13 - Lire les valeurs de  a et  b
+a = int(input("Tapez la valeur de l'entier  a  :  "))
+b = int(input("Tapez la valeur de l'entier  b  :  "))
+q = a//b
+r = a%b
+print("Le quotient de la division euclidienne de a par b est  : q = ", q)
+print("Le reste de la division euclidienne de a par b est : r = ", r)
+
+
+# 14 - Lire la valeur de l'entier n
+n = int(input("Tapez la valeur de n :  "))
+# On utilise un compteur j 
+j = 0
+for i in range(0,n):
+    if(i**2 == n):
+        j = j +1
+if(j > 0):
+    print("L'entier  ", n , " est un carré parfait")
+else:
+    print("l'entier ", n , " n'est pas est un carré parfait")
